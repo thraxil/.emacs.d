a823ccf714feddbcdfa19b429d4396baf0ec6266
Anders Pearson <anders@columbia.edu>
2015-08-22 18:08:37 +0200

    split out magit config

--- init.el
+++ init.el
@@ -57,18 +57,7 @@ re-downloaded in order to locate PACKAGE."
 
 ;;;;;;;;;;;;;;;;;;; global settings ;;;;;;;;;;;;;;;;;;;;;;
 
-;; full screen magit-status
-
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
+(eval-after-load "magit" '(require 'setup-magit))
 
 ;; (global-whitespace-mode 1)
 
