19e2d65e153dd01132c07ef1acbd82851b187a57
Anders Pearson <anders@columbia.edu>
2015-08-30 21:50:41 +0200

    don't recenter cursor in eshell-mode

--- init.el
+++ init.el
@@ -169,7 +169,8 @@ Including indent-buffer, which should not be called automatically on save."
 ;; keep point centered vertically
 (add-hook 'post-command-hook
           (lambda ()
-            (recenter '("don't redraw"))))
+            (unless (eq major-mode 'eshell-mode)
+									(recenter '("don't redraw")))))
 
 ;;;;;;;;;;;;;;;;;;; modes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -273,6 +274,31 @@ Including indent-buffer, which should not be called automatically on save."
 
 
 ;;;;;;;;;;;;;;;;;;; extra functions ;;;;;;;;;;;;;;;;;;;;;;
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
 
 (defun kill-syntax-forward ()
   "Kill characters with syntax at point."
